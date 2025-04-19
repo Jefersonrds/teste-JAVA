783a884f25db2a1834fb6a3a361454b6ba4d1a5e
Jefersonrds <contatojefersonrds@gmail.com>
2025-04-19 12:10:33 -0300

    Tratar erros

---
+++ testesJava/src/matematica.java
@@ -0,0 +1,5 @@
+public class matematica {
+    public void dividir (int numeroA, int numeroB){
+        System.out.println(numeroA/numeroB);
+    }
+}
---
+++ testesJava/src/testesJava.java
@@ -0,0 +1,34 @@
+import javax.swing.JOptionPane;
+
+public class testesJava {
+
+    public static void main(String[] args) {
+        
+        int[] numeros = new int[] {10,20,30, 100};
+        int[] peso = new int[] {2,0,4};
+        matematica matematica = new matematica();
+                    
+        for(int indice = 0;indice < 4;indice++){
+    
+            try{ //tentar
+                
+                matematica.dividir(numeros[indice], peso[indice]);
+                System.out.println("Mensagem apos a excecao");
+
+            }catch(ArithmeticException erro){ //capturar
+                // Somente trata os erros de aritimetico de calculos matematicos
+                
+                JOptionPane.showMessageDialog(null, "Erro de calculo: " + erro.getMessage());
+                //System.out.println("Erro de calculo: " + erro.getMessage());
+            // catch (<ClasseDaexcecao)
+            }catch(ArrayIndexOutOfBoundsException erro){
+                //Trata os erros relacionados ao ultrapassar o tamanho do vetor
+                System.out.println("Erro de vetor: " + erro.getMessage());
+                
+                //Para ver a classe do erro: erro.printStackTrace();
+            }
+
+        }
+        System.out.println("Mensagem final.");
+    }
+}
